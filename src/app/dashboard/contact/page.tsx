b43a53c5d1781e35461d0b28fcbb6fe939405d99
MOHAMMAD NADEEM <mnadeem.mn838@gmail.com>
2024-03-18 16:51:47 +0530

    changes in form component

--- src/app/dashboard/contact/page.tsx
+++ src/app/dashboard/contact/page.tsx
@@ -3,11 +3,19 @@
 import type { NextPage } from "next";
 import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Grid,
+  Input,
+  Textarea,
+} from "@chakra-ui/react";
 
 const Contact: NextPage = () => {
   const form = useRef();
 
-  const sendEmail = (e: { preventDefault: () => void; }) => {
+  const sendEmail = (e: { preventDefault: () => void }) => {
     e.preventDefault();
 
     emailjs
@@ -24,14 +32,35 @@ const Contact: NextPage = () => {
       );
   };
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
+    // <form ref={form} onSubmit={sendEmail}>
+    //   <label>Name</label>
+    //   <input type="text" name="user_name" />
+    //   <label>Email</label>
+    //   <input type="email" name="user_email" />
+    //   <label>Message</label>
+    //   <textarea name="message" />
+    //   <input type="submit" value="Send" />
+    // </form>
+
+    <form ref={form} onSubmit={sendEmail} className="w-1/2">
+      <FormControl>
+        <FormLabel>Name</FormLabel>
+        <Input type="text" name="user_name" />
+      </FormControl>{" "}
+      <br />
+      <FormControl>
+        <FormLabel>Email</FormLabel>
+        <Input type="email" name="user_email" />
+      </FormControl>{" "}
+      <br />
+      <FormControl>
+        <FormLabel>Message</FormLabel>
+        <Textarea name="message" rows={8} />
+      </FormControl>{" "}
+      <br />
+      <Button colorScheme="blue" type="submit">
+        Send
+      </Button>
     </form>
   );
 };
